4aa8a7d6fa7deddeb31aca4419821b52b99874ea
AndrewFeran <aferan@proton.me>
2025-10-23 14:45:53 -0400

    init session 2

---
+++ Session_2/arrays_unfinished.c
@@ -0,0 +1,15 @@
+/*
+    10/23/2025
+    
+    We want to create a program that can take some user inputs from the user,
+    load them into an array, print the array, and do some operations.
+
+    If time permits we may use characters
+*/
+
+#include <iostream>
+#define SIZE 50
+
+int main() {
+
+}
